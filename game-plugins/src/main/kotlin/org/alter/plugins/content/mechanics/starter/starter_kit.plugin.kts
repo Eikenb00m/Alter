0434c9737e7514c83d05d799d9ac684ab7b67287
J. Govers <31247938+Eikenb00m@users.noreply.github.com>
2024-10-12 14:40:59 +0200

    Change starter magic book

--- game-plugins/src/main/kotlin/org/alter/plugins/content/mechanics/starter/starter_kit.plugin.kts
+++ game-plugins/src/main/kotlin/org/alter/plugins/content/mechanics/starter/starter_kit.plugin.kts
@@ -5,12 +5,9 @@ import org.alter.game.model.attr.NEW_ACCOUNT_ATTR
 on_login {
     if (player.attr[NEW_ACCOUNT_ATTR] ?: return@on_login) {
         with(player.inventory) {
-            add(Items.LOGS, 5)
-            add(Items.TINDERBOX)
-            add(Items.BREAD, 5)
-            add(Items.BRONZE_PICKAXE)
+            add(Items.BRONZE_SWORD)
             add(Items.BRONZE_DAGGER)
-            add(Items.KNIFE)
         }
+        player.setVarbit(4070, 1)
     }
 }
