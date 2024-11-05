e228d92652aaf40e7d175cad19e775bcd83d488e
J. Govers <31247938+Eikenb00m@users.noreply.github.com>
2024-11-05 18:37:29 +0100

    Fix starter

--- game-plugins/src/main/kotlin/org/alter/plugins/content/mechanics/starter/starter_kit.plugin.kts
+++ game-plugins/src/main/kotlin/org/alter/plugins/content/mechanics/starter/starter_kit.plugin.kts
@@ -6,7 +6,8 @@ on_login {
     if (player.attr[NEW_ACCOUNT_ATTR] ?: return@on_login) {
         with(player.inventory) {
             add(Items.BRONZE_SWORD)
-            add(Items.BRONZE_DAGGER)
+            add(Items.WOODEN_SHIELD)
+            add(Items.COINS_995, 25)
         }
         player.setVarbit(4070, 1)
     }
