9b255d5c155b0fe8e64539bb5c50366ace3cd083
J. Govers <31247938+Eikenb00m@users.noreply.github.com>
2024-11-06 15:44:26 +0100

    Create mage-east.spawns.plugin.kts

---
+++ game-plugins/src/main/kotlin/org/alter/plugins/content/area/yanille/spawns/mage-east.spawns.plugin.kts
@@ -0,0 +1,10 @@
+package org.alter.plugins.content.area.yanille.spawns
+
+/**Battle Mages*/
+spawn_npc(Npcs.BATTLE_MAGE_1611, 2629, 3087,0, 3, direction = Direction.NORTH)
+spawn_npc(Npcs.BATTLE_MAGE_1611, 2626, 3085,0, 3, direction = Direction.NORTH)
+spawn_npc(Npcs.BATTLE_MAGE_1611, 2630, 3084,0, 3, direction = Direction.NORTH)
+spawn_npc(Npcs.BATTLE_MAGE_1611, 2627, 3081,0, 3, direction = Direction.NORTH)
+spawn_npc(Npcs.BATTLE_MAGE_1611, 2623, 3081,0, 3, direction = Direction.NORTH)
+/**Avatar of Magic */
+spawn_npc(Npcs.ARZINIAN_AVATAR_OF_MAGIC_1235, 2628, 3076,0, 5, direction = Direction.NORTH)
