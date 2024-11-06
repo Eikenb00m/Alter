9d4eda6c7330aec80b71864f45bdd75b444e6dd3
J. Govers <31247938+Eikenb00m@users.noreply.github.com>
2024-11-06 15:37:42 +0100

    Create cow.spawns.plugin.kts

---
+++ game-plugins/src/main/kotlin/org/alter/plugins/content/area/yanille/spawns/cow.spawns.plugin.kts
@@ -0,0 +1,15 @@
+package org.alter.plugins.content.area.yanille.spawns
+
+/**Giant Rats*/
+spawn_npc(Npcs.COW, 2608, 3113,0, 3, direction = Direction.EAST)
+spawn_npc(Npcs.COW, 2608, 3115,0, 3, direction = Direction.EAST)
+spawn_npc(Npcs.COW, 2608, 3117,0, 3, direction = Direction.EAST)
+spawn_npc(Npcs.COW, 2608, 3119,0, 3, direction = Direction.EAST)
+spawn_npc(Npcs.COW, 2606, 3113,0, 3, direction = Direction.EAST)
+spawn_npc(Npcs.COW, 2606, 3115,0, 3, direction = Direction.EAST)
+spawn_npc(Npcs.COW, 2606, 3117,0, 3, direction = Direction.EAST)
+spawn_npc(Npcs.COW, 2606, 3119,0, 3, direction = Direction.EAST)
+spawn_npc(Npcs.COW, 2604, 3113,0, 3, direction = Direction.EAST)
+spawn_npc(Npcs.COW, 2604, 3115,0, 3, direction = Direction.EAST)
+spawn_npc(Npcs.COW, 2604, 3117,0, 3, direction = Direction.EAST)
+spawn_npc(Npcs.COW, 2604, 3119,0, 3, direction = Direction.EAST)
