16a14207395c89d7500170aae189c223a6e48a19
J. Govers <31247938+Eikenb00m@users.noreply.github.com>
2024-11-06 15:31:38 +0100

    All Yanille spawns
    All spawn between city walls

--- game-plugins/src/main/kotlin/org/alter/plugins/content/area/yanille/spawns/spawns.plugin.kts
+++ game-plugins/src/main/kotlin/org/alter/plugins/content/area/yanille/spawns/spawns.plugin.kts
@@ -6,13 +6,56 @@ spawn_npc(npc = Npcs.BANKER_1618, x = 2615, z = 3092, direction = Direction.WEST
 spawn_npc(npc = Npcs.BANKER_1613, x = 2615, z = 3091, direction = Direction.WEST)
 
 /**Stores*/
-spawn_npc(Npcs.AUBURY_11435, 2594, 3105, 0, 2)
-spawn_npc(Npcs.SHOP_KEEPER, 2595, 3105,0, 2)
-spawn_npc(Npcs.SHOP_ASSISTANT, 2597, 3104,0, 2)
-spawn_npc(Npcs.ARMOUR_SALESMAN, 2598, 3040,0, 2)
+spawn_npc(Npcs.AUBURY_11435, 2594, 3105, 0, 2, direction = Direction.SOUTH)
+spawn_npc(Npcs.SHOP_KEEPER, 2595, 3105,0, 2, direction = Direction.SOUTH)
+spawn_npc(Npcs.SHOP_ASSISTANT, 2597, 3104,0, 2, direction = Direction.SOUTH)
+spawn_npc(Npcs.ARMOUR_SALESMAN, 2598, 3040,0, 2, direction = Direction.SOUTH)
 
 /**Makeovermage*/
-spawn_npc(Npcs.MAKEOVER_MAGE, 2602, 3088,0, 3)
+spawn_npc(Npcs.MAKEOVER_MAGE, 2602, 3088,0, 3, direction = Direction.EAST)
 
 /**Wizards Guild Spawns*/
-spawn_npc(Npcs.ARCHMAGE_SEDRIDOR_11433, 2591, 3086,0, 3)
+spawn_npc(Npcs.ARCHMAGE_SEDRIDOR_11433, 2591, 3086,0, 3, direction = Direction.EAST)
+
+/**Giant Rats*/
+spawn_npc(Npcs.GIANT_RAT_2858, 2610, 3103,0, 3, direction = Direction.SOUTH)
+spawn_npc(Npcs.GIANT_RAT_2858, 2611, 3103,0, 3, direction = Direction.SOUTH)
+spawn_npc(Npcs.GIANT_RAT_2858, 2612, 3103,0, 3, direction = Direction.SOUTH)
+spawn_npc(Npcs.GIANT_RAT_2858, 2613, 3103,0, 3, direction = Direction.SOUTH)
+spawn_npc(Npcs.GIANT_RAT_2858, 2614, 3103,0, 3, direction = Direction.SOUTH)
+
+/**Yanille Soldiers*/
+spawn_npc(Npcs.SOLDIER_5421, 2600, 3104,0, 3, direction = Direction.SOUTH)
+spawn_npc(Npcs.SOLDIER_5421, 2603, 3103,0, 3, direction = Direction.SOUTH)
+spawn_npc(Npcs.SOLDIER_5421, 2600, 3101,0, 3, direction = Direction.SOUTH)
+spawn_npc(Npcs.SOLDIER_5421, 2603, 3101,0, 3, direction = Direction.SOUTH)
+
+/**Farmers*/
+spawn_npc(Npcs.FARMER_3114, 2579, 3104,0, 5, direction = Direction.SOUTH)
+spawn_npc(Npcs.FARMER_3114, 2572, 3105,0, 5, direction = Direction.SOUTH)
+spawn_npc(Npcs.MASTER_FARMER_5730, 2566, 3098,0, 10, direction = Direction.EAST)
+
+/**Watchman*/
+spawn_npc(Npcs.WATCHMAN_5420, 2588, 3106,0, 3, direction = Direction.SOUTH)
+spawn_npc(Npcs.WATCHMAN_5420, 2588, 3104,0, 3, direction = Direction.SOUTH)
+spawn_npc(Npcs.WATCHMAN_5420, 2588, 3102,0, 3, direction = Direction.SOUTH)
+spawn_npc(Npcs.WATCHMAN_5420, 2586, 3106,0, 3, direction = Direction.SOUTH)
+spawn_npc(Npcs.WATCHMAN_5420, 2586, 3104,0, 3, direction = Direction.SOUTH)
+spawn_npc(Npcs.WATCHMAN_5420, 2586, 3102,0, 3, direction = Direction.SOUTH)
+
+/**Mossgiants*/
+spawn_npc(Npcs.MOSS_GIANT, 2578, 3083,0, 3, direction = Direction.NORTH)
+spawn_npc(Npcs.MOSS_GIANT, 2575, 3085,0, 3, direction = Direction.NORTH)
+spawn_npc(Npcs.MOSS_GIANT, 2572, 3082,0, 3, direction = Direction.NORTH)
+spawn_npc(Npcs.MOSS_GIANT, 2576, 3076,0, 3, direction = Direction.NORTH)
+spawn_npc(Npcs.MOSS_GIANT, 2580, 3081,0, 3, direction = Direction.NORTH)
+spawn_npc(Npcs.MOSS_GIANT, 2582, 3076,0, 3, direction = Direction.NORTH)
+
+/**Heros*/
+spawn_npc(Npcs.HERO_3295, 2540, 3089,0, 3, direction = Direction.EAST)
+spawn_npc(Npcs.HERO_3295, 2542, 3089,0, 3, direction = Direction.EAST)
+spawn_npc(Npcs.HERO_3295, 2540, 3093,0, 3, direction = Direction.EAST)
+spawn_npc(Npcs.HERO_3295, 2542, 3093,0, 3, direction = Direction.EAST)
+
+/**Dad*/
+spawn_npc(Npcs.DAD_6391, 2544, 3095,0, 3, direction = Direction.SOUTH)
