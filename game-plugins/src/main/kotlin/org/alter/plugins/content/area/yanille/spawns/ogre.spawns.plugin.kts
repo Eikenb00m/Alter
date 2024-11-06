0bf898217c3335b6ad0f3f100b7fa6f7e554ad4f
J. Govers <31247938+Eikenb00m@users.noreply.github.com>
2024-11-06 15:57:01 +0100

    Create ogre.spawns.plugin.kts

---
+++ game-plugins/src/main/kotlin/org/alter/plugins/content/area/yanille/spawns/ogre.spawns.plugin.kts
@@ -0,0 +1,13 @@
+package org.alter.plugins.content.area.yanille.spawns
+
+/**Ogre*/
+spawn_npc(Npcs.OGRE, 2511, 3115,0, 3, direction = Direction.EAST)
+spawn_npc(Npcs.OGRE, 2507, 3117,0, 3, direction = Direction.EAST)
+spawn_npc(Npcs.OGRE, 2507, 3121,0, 3, direction = Direction.EAST)
+spawn_npc(Npcs.OGRE, 2503, 3119,0, 3, direction = Direction.EAST)
+spawn_npc(Npcs.OGRE, 2502, 3114,0, 3, direction = Direction.EAST)
+spawn_npc(Npcs.OGRE, 2506, 3113,0, 3, direction = Direction.EAST)
+spawn_npc(Npcs.OGRE, 2502, 3109,0, 3, direction = Direction.EAST)
+spawn_npc(Npcs.OGRE, 2505, 3108,0, 3, direction = Direction.EAST)
+spawn_npc(Npcs.OGRE, 2508, 3108,0, 3, direction = Direction.EAST)
+spawn_npc(Npcs.OGRE, 2507, 3103,0, 3, direction = Direction.EAST)
