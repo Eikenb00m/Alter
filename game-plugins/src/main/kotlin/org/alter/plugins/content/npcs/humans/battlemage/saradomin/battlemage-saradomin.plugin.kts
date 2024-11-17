f0e7e67ad0586e07c967d14865bf37d7ee9beaae
J. Govers <31247938+Eikenb00m@users.noreply.github.com>
2024-11-17 13:18:09 +0100

    Create battlemage-saradomin.plugin.kts

---
+++ game-plugins/src/main/kotlin/org/alter/plugins/content/npcs/humans/battlemage/saradomin/battlemage-saradomin.plugin.kts
@@ -0,0 +1,53 @@
+package org.alter.plugins.content.npcs.humans.battlemage.saradomin
+
+import org.alter.plugins.content.combat.isBeingAttacked
+
+/**
+ *  @author <a href="https://github.com/CloudS3c">Cl0ud</a>
+ *  @author <a href="https://www.rune-server.ee/members/376238-cloudsec/">Cl0ud</a>
+ *
+ */
+val cow_npc_list =
+    listOf(
+        Npcs.BATTLE_MAGE_1611,
+    )
+
+
+cow_npc_list.forEach {
+    set_combat_def(it) {
+        configs {
+            attackSpeed = 4
+            respawnDelay = 150
+            poisonChance = 0.0
+            venomChance = 0.0
+        }
+        stats {
+            hitpoints = 120
+            attack = 1
+            strength = 1
+            defence = 1
+            magic = 50
+            ranged = 1
+        }
+
+        bonuses {
+            defenceStab = 0
+            defenceSlash = 0
+            defenceCrush = 0
+            defenceMagic = 0
+            defenceRanged = 0
+        }
+
+        anims {
+            attack = Animation.HUMAN_STAFF_BASH
+            block = Animation.HUMAN_STAFF_DEFEND
+            death = Animation.HUMAN_DEATH
+        }
+
+        sound {
+            attackSound = Sound.FARMERSFORK_STAB
+            blockSound = Sound.HUMAN_HIT
+            deathSound = Sound.HUMAN_DEATH
+        }
+    }
+}
